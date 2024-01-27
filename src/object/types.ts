449d8db59d9a83526b3524a17001ad86ba60c060
Diep Dang <kdt310722@gmail.com>
2024-01-27 22:48:48 +0700

    feat(object): supported number key type in FlattenKeys type

--- src/object/types.ts
+++ src/object/types.ts
@@ -6,7 +6,7 @@ export type PickByType<T, Value> = {
 
 export type FilterPredicate<O, K extends keyof O> = (key: K, value: O[K], index: number) => boolean
 
-export type FlattenKeys<T, D extends string = '.'> = T extends AnyObject ? { [K in keyof T]: `${string & K}${FlattenKeys<T[K], D> extends never ? '' : `${D}${FlattenKeys<T[K], D>}`}` }[keyof T] : never
+export type FlattenKeys<T, D extends string = '.'> = T extends AnyObject ? { [K in keyof T]: `${Exclude<K, symbol>}${FlattenKeys<T[K], D> extends never ? '' : `${D}${FlattenKeys<T[K], D>}`}` }[keyof T] : never
 
 export type FollowPath<T, P, D extends string = '.'> = P extends `${infer U}${D}${infer R}` ? U extends keyof T ? FollowPath<T[U], R, D> : never : P extends keyof T ? T[P] : never
 
