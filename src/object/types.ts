4dc06060ad8657987a492739ab90e3d99c4e73c5
Diep Dang <kdt310722@gmail.com>
2024-01-27 22:20:35 +0700

    feat(object): add some type utils to flatten an object

--- src/object/types.ts
+++ src/object/types.ts
@@ -5,3 +5,11 @@ export type PickByType<T, Value> = {
 }
 
 export type FilterPredicate<O, K extends keyof O> = (key: K, value: O[K], index: number) => boolean
+
+export type FlattenKeys<T, D extends string = '.'> = T extends AnyObject ? { [K in keyof T]: `${string & K}${FlattenKeys<T[K], D> extends never ? '' : `${D}${FlattenKeys<T[K], D>}`}` }[keyof T] : never
+
+export type FollowPath<T, P, D extends string = '.'> = P extends `${infer U}${D}${infer R}` ? U extends keyof T ? FollowPath<T[U], R, D> : never : P extends keyof T ? T[P] : never
+
+export type Flatten<O extends AnyObject, D extends string = '.'> = {
+    [P in FlattenKeys<O, D>]: FollowPath<O, P, D>
+}
