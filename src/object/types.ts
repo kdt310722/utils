30bdbbe3c0614206b7f96081637e20e522a52b86
Diep Dang <kdt310722@gmail.com>
2024-01-28 00:27:45 +0700

    feat(object): add some type utils to help set the property value by path

--- src/object/types.ts
+++ src/object/types.ts
@@ -8,7 +8,13 @@ export type FilterPredicate<O, K extends keyof O> = (key: K, value: O[K], index:
 
 export type FlattenKeys<T, D extends string = '.'> = T extends AnyObject ? { [K in keyof T]: `${Exclude<K, symbol>}${FlattenKeys<T[K], D> extends never ? '' : `${D}${FlattenKeys<T[K], D>}`}` }[keyof T] : never
 
-export type GetValue<T, P, D extends string = '.'> = P extends `${infer U}${D}${infer R}` ? U extends keyof T ? GetValue<T[U], R, D> : never : P extends keyof T ? T[P] : never
+export type GetValue<T, P, D extends string = '.'> = P extends `${infer U}${D}${infer R}` ? (U extends keyof T ? GetValue<T[U], R, D> : never) : (P extends keyof T ? T[P] : never)
+
+export type FromPath<P extends PropertyKey, V, D extends string = '.'> = P extends `${infer U}${D}${infer R}` ? { [K in U]: FromPath<R, V, D> } : { [K in P]: V }
+
+export type SetValue<O extends AnyObject, P extends PropertyKey, V> = P extends keyof O ? { [K in keyof O]: K extends P ? V : O[K] } : (O & { [K in P]: V })
+
+export type SetValueByPath<O extends AnyObject, P extends PropertyKey, V, D extends string = '.'> = P extends `${infer U}${D}${infer R}` ? U extends keyof O ? { [K in keyof O]: K extends U ? O[K] extends AnyObject ? SetValueByPath<O[K], R, V, D> : FromPath<R, V, D> : O[K] } : (O & { [K in U]: FromPath<R, V, D> }) : SetValue<O, P, V>
 
 export type Flatten<O extends AnyObject, D extends string = '.'> = {
     [P in FlattenKeys<O, D>]: GetValue<O, P, D>
