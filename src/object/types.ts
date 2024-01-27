1cebeb0b96005b2936ec3557495df895e391d829
Diep Dang <kdt310722@gmail.com>
2024-01-27 22:49:56 +0700

    refactor(object): rename FollowPath to GetValue

--- src/object/types.ts
+++ src/object/types.ts
@@ -8,8 +8,8 @@ export type FilterPredicate<O, K extends keyof O> = (key: K, value: O[K], index:
 
 export type FlattenKeys<T, D extends string = '.'> = T extends AnyObject ? { [K in keyof T]: `${Exclude<K, symbol>}${FlattenKeys<T[K], D> extends never ? '' : `${D}${FlattenKeys<T[K], D>}`}` }[keyof T] : never
 
-export type FollowPath<T, P, D extends string = '.'> = P extends `${infer U}${D}${infer R}` ? U extends keyof T ? FollowPath<T[U], R, D> : never : P extends keyof T ? T[P] : never
+export type GetValue<T, P, D extends string = '.'> = P extends `${infer U}${D}${infer R}` ? U extends keyof T ? GetValue<T[U], R, D> : never : P extends keyof T ? T[P] : never
 
 export type Flatten<O extends AnyObject, D extends string = '.'> = {
-    [P in FlattenKeys<O, D>]: FollowPath<O, P, D>
+    [P in FlattenKeys<O, D>]: GetValue<O, P, D>
 }
