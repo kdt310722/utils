9eed2ceb3f9c35c8e61eb4a731b56a7790c6cce3
Diep Dang <kdt310722@gmail.com>
2024-03-09 23:48:58 +0700

    fix(promise/timeout): clear timer when promise throw an error before timeout reached

--- src/promise/timeout.ts
+++ src/promise/timeout.ts
@@ -1,9 +1,10 @@
-export function withTimeout<T>(promise: Promise<T>, ms: number, message?: Error | string): Promise<T> {
-    return new Promise((resolve, reject) => {
+export function withTimeout<T>(promise: Promise<T>, ms: number, message?: Error | string) {
+    return new Promise<T>((resolve, reject) => {
         const timeoutId = setTimeout(() => reject(message instanceof Error ? message : new Error(message ?? `Promise timed out after ${ms} ms`)), ms)
 
-        promise.then(resolve).catch(reject).finally(() => {
+        promise.then(resolve).finally(() => clearTimeout(timeoutId)).catch((error) => {
             clearTimeout(timeoutId)
+            reject(error as Error)
         })
     })
 }
