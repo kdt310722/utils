371dadbb4264b2053decde171d8bbd9f2058f5aa
Diep Dang <kdt310722@gmail.com>
2024-03-09 23:51:29 +0700

    fix(promise/timeout): support add custom timeout error using function

--- src/promise/timeout.ts
+++ src/promise/timeout.ts
@@ -1,6 +1,18 @@
-export function withTimeout<T>(promise: Promise<T>, ms: number, message?: Error | string) {
+import { isFunction } from '../function'
+
+export function withTimeout<T>(promise: Promise<T>, ms: number, message?: Error | (() => Error) | string) {
     return new Promise<T>((resolve, reject) => {
-        const timeoutId = setTimeout(() => reject(message instanceof Error ? message : new Error(message ?? `Promise timed out after ${ms} ms`)), ms)
+        const timeoutId = setTimeout(() => {
+            if (isFunction(message)) {
+                return reject(message())
+            }
+
+            if (message instanceof Error) {
+                return reject(message)
+            }
+
+            reject(new Error(message ?? `Promise timed out after ${ms} ms`))
+        }, ms)
 
         promise.then(resolve).finally(() => clearTimeout(timeoutId)).catch((error) => {
             clearTimeout(timeoutId)
