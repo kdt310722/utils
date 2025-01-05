b60e2a1cc7b077e655a65062f760d190131b1e45
Diep Dang <kdt310722@gmail.com>
2025-01-05 20:46:05 +0700

    feat: wait until next second

--- src/promise/sleep.ts
+++ src/promise/sleep.ts
@@ -1,1 +1,5 @@
 export const sleep = async (ms: number) => new Promise<void>((resolve) => setTimeout(resolve, ms))
+
+export async function waitUntilNextSecond() {
+    return new Promise<void>((resolve) => setTimeout(resolve, 1000 - (Date.now() % 1000)))
+}
