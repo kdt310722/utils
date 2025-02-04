16a4d8c1410f98a3f01f28b00726187ca0cb3762
Diep Dang <kdt310722@gmail.com>
2025-02-04 21:27:32 +0700

    feat(promise): add "safeRace" util

--- src/promise/index.ts
+++ src/promise/index.ts
@@ -1,7 +1,9 @@
 export * from './deferred'
 export * from './lock'
 export * from './poll'
+export * from './race'
 export * from './retry'
 export * from './sleep'
 export * from './timeout'
+
 export type * from './types'
