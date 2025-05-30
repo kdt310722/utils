8a3b21257088287b379610a7937f77121055464d
DiepPk <kdt310722@gmail.com>
2025-05-30 10:57:39 +0700

    refactor: rename tap to pTap and update export

--- src/promise/tap.ts
+++ src/promise/tap.ts
@@ -1,10 +1,12 @@
 import type { Awaitable } from './types'
 
-export const tap = <T>(fn: (value: T) => Awaitable<unknown>) => async (value: T): Promise<T> => {
+export const pTap = <T>(fn: (value: T) => Awaitable<unknown>) => async (value: T): Promise<T> => {
     return Promise.resolve(fn(value)).then(() => value)
 }
 
-tap.catch = (fn: (error: unknown) => Awaitable<unknown>) => async (error: unknown): Promise<never> => {
+pTap.catch = (fn: (error: unknown) => Awaitable<unknown>) => async (error: unknown): Promise<never> => {
     await fn(error)
     throw error
 }
+
+export const tap = pTap
